95204096e8352ea46720f8f2e97d641e237110b6
Andrei Crisnic <andrei.crisnic-cic.netherlands@ibm.com>
2021-12-09 13:20:40 +0100

    Crowning the winner fish

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const fish = document.getElementsByClassName("fish");
 const finishLine = document.getElementById("finish-line");
+
+let countDown = true;
+let startGame = false;
+
 let currentMargin = 0;
 let counter = 0;
 let win = false;
-
-console.log(finishLine);
+const winners = [];
+let winner;
 
 const scrollOffsets = (function () {
   const w = window;
@@ -22,24 +26,46 @@ const scrollOffsets = (function () {
 const finishLineOffset =
   finishLine.getBoundingClientRect().left + scrollOffsets.x;
 
-const gameInterval = setInterval(() => {
-  Array.from(fish).forEach((fishInstance) => {
-    //Start fish with 0 margin so they start right at the beggining
-    fishInstance.style.marginLeft = `${currentMargin}px`;
-    let random = Math.random();
-    if (random < 0.5) {
-      currentMargin -= 60;
-    }
-    currentMargin += 50;
-    fishInstance.style.marginLeft = `${currentMargin}px`;
+if (startGame) {
+  const gameInterval = setInterval(() => {
+    Array.from(fish).forEach((fishInstance) => {
+      //Start fish with 0 margin so they start right at the beggining
+      fishInstance.style.marginLeft = `${currentMargin}px`;
 
-    const currentLeftMargin = parseFloat(fishInstance.style["margin-left"]);
-    const naturalWidth = fishInstance.naturalWidth;
-    if (currentLeftMargin + naturalWidth > finishLineOffset) {
-      win = true;
+      calculateNewMargin(fishInstance);
+
+      const currentLeftMargin = parseFloat(fishInstance.style["margin-left"]);
+      const { offsetWidth } = fishInstance;
+      if (currentLeftMargin + offsetWidth >= finishLine.offsetLeft) {
+        win = true;
+        /*
+      Add all the fish that pass the finish line to an array, 
+      because there might be a chance that more of them will pass the finish line until the logic ends
+      so I always get the fish that enters the array first
+      */
+        winners.push(fishInstance);
+      }
+    });
+    if (win) {
+      clearInterval(gameInterval);
+      winner = winners[0];
+      crownKing(winner);
     }
-  });
-  if (win) {
-    clearInterval(gameInterval);
+  }, 500);
+}
+
+const crownKing = (winnerFish) => {
+  const crownImage = document.createElement("img");
+  crownImage.src = "./assets/crown.png";
+  crownImage.classList.add("crown");
+  winnerFish.insertBefore(crownImage, winnerFish.firstChild);
+};
+
+const calculateNewMargin = (fish) => {
+  let random = Math.random();
+  if (random < 0.5) {
+    currentMargin -= 60;
   }
-}, 500);
+  currentMargin += 40;
+  fish.style.marginLeft = `${currentMargin}px`;
+};
